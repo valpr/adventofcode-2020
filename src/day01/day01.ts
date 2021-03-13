88ef2a62930a8e74eee2a656763f76c89b476f30
valpr <me@andrewfong.ca>
2021-03-12 22:12:14 -0500

    day1

--- src/day01/day01.ts
+++ src/day01/day01.ts
@@ -2,8 +2,16 @@ import { readFileSync } from 'fs';
 
 const input = readFileSync('./input.txt', 'utf-8').split('\n').map(num => parseInt(num));
 
-var dict = {};
+const dict: { [neededNum: number]: number} = {};
 const target = 2020;
+const answer = input.find(num => {
+    if (dict[num]){
+        return true;
+    }
+    else{
+        dict[target-num] = num;
+    }
+})
 
-
-console.log(input);
+if (answer)
+    console.log(answer * (2020 - answer));
