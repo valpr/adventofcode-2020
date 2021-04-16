5714ceecca8bee8bce316ebd4bcb4d7e9b5aa6e1
valpr <me@andrewfong.ca>
2021-04-15 23:06:44 -0400

    part1 day20

--- src/day20/part01.ts
+++ src/day20/part01.ts
@@ -1,8 +1,6 @@
 import {readFileSync} from 'fs';
-let tiles: string[] = readFileSync('./input.txt', 'utf-8').split('\r\n\r\n').map(tile => tile.split(':\r\n')[1]);
+let tiles: string[] = readFileSync('./input.txt', 'utf-8').split('\r\n\r\n');
 //basically need to match tiles to other tiles based on #'s on the borders
-console.log(tiles);
-console.log(tiles[0]);
 //in order for 2 edges to possibly match
 //same number of #'s, in the same coordinates
 //however, tiles were flipped or rotated, so it could be anything
@@ -15,3 +13,83 @@ console.log(tiles[0]);
 //compareEdge: compare array to another array, then flip one array and compare again\
 //compareSquare: compare all 4 arrays against 4 other arrays, only compare if #'s are equal
 //probably want to represent squares
+type Edge = { //Add # Number?
+    fromID: number;
+    representation: string;
+    reverseRepresentation: string;
+    count: number;
+}
+
+type Square = {
+    ID: number;
+    connectedTo: number[];
+    matchedEdges: Edge[];
+    unmatchedEdges: Edge[];
+}
+
+type EdgeCollection = {
+    [key: string]: number[]; //leads to squareID
+}
+
+const allEdges: EdgeCollection = {};
+
+const parseSquare = (tile: string):Square => {
+    //need to return a square object with 4 edges
+    //add edges to edge collection
+    const [ID, square] = tile.split(':\r\n');
+    const lines = square.split('\r\n');
+    const edges = parseEdges(lines, parseInt(ID.split(' ')[1]));
+    return {
+        ID: parseInt(ID.split(' ')[1]), connectedTo: [], matchedEdges: [], unmatchedEdges: edges
+    }
+}
+
+const parseEdges = (tileArray: string[], ID: number): Edge[] => {
+    let sides = [getSide(tileArray, 0), getSide(tileArray, tileArray[0].length-1), tileArray[0], tileArray[tileArray.length-1]];
+    let edges :Edge[] = sides.map((side) =>  {
+        let reverseSide = side.split('').reverse().join('');
+        let count = side.split('').filter(x => x === '#').length
+        if (allEdges[side]){
+            allEdges[side] = allEdges[side].concat([ID])
+        } else if (allEdges[reverseSide]) {
+            allEdges[reverseSide] = allEdges[reverseSide].concat([ID]);
+        } else {
+            allEdges[side] = [ID];
+        }
+        
+        return { representation: side, reverseRepresentation: reverseSide, fromID: ID, count }
+    })
+    return edges
+}
+
+const getSide = (tileArray: string[], index: number): string => {
+    if (tileArray.length === 0)
+        return ''
+    return tileArray[0][index] + getSide(tileArray.slice(1), index);
+}
+
+
+let sqArray: Square[] = [];
+for (let rep of tiles) {
+    sqArray.push(parseSquare(rep));
+}
+
+let nums = [];
+let finalNum = 1;
+let numTimesReferenced : {[key: number]: number}= {}
+for (let key of Object.keys(allEdges)) {
+    if (allEdges[key].length === 1){
+        let ID = allEdges[key][0];
+        numTimesReferenced[ID] = numTimesReferenced[ID] ? numTimesReferenced[ID]+1 : 1;
+        if (numTimesReferenced[ID] === 2) {
+            nums.push(ID);
+            finalNum *= ID;
+        }
+    }
+}
+
+console.log(nums);
+console.log(finalNum);
+//TODO: loop through dictionary values
+//if length is 1, add to dictionary
+//if upon adding to dictionary, value is 2, add to final array
